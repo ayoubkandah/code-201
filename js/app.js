92d2fb54e2b89bf1c9f7db19acbdf312ca30ba2a
ayoubkandah <a.kandah994@gmail.com>
2021-01-05 21:33:58 +0200

    changing in quiz

--- js/app.js
+++ js/app.js
@@ -1,24 +1,53 @@
 
 function doscript(){
       'use strict'
+      var name = prompt("whats your name?")
+      var grade=0;
+      var answer1=prompt("i am a doctor ? Y/N ")
+while(answer1.toLowerCase()!=="n" && !answer1.toLowerCase()!=="no")
+{
+    alert("wrong answer")
+    var answer1=prompt("i am a doctor ? Y/N ")
 
-var name = prompt("whats your name?")
-var answer1=prompt("i am a doctor ? Y/N ")
-var answer2=prompt("my name is ayoub ? Y/N ")
-var answer3=prompt("I dont know anything about unity engine ? Y/N ")
-var answer4=prompt("I am a game developer ? Y/N ")
-var answer5=prompt("its not ayoub website ? Y/N ")
-var grade=0;
+}
+
 if(answer1.toLowerCase()=="n" ||answer1.toLowerCase()=="no" ){
 
     grade++;
-} if (answer2.toLowerCase()=="y"||answer2.toLowerCase()=="yes"){
+}
+var answer2=prompt("my name is ayoub ? Y/N ")
+ while(answer2.toLowerCase()!=="y" && answer2.toLowerCase()!=="yes"){
+    alert("wrong answer")
+    answer2=prompt("my name is ayoub ? Y/N ")
+
+}
+if (answer2.toLowerCase()=="y"||answer2.toLowerCase()=="yes"){
     grade++;
-} if (answer3.toLowerCase()=="n" || answer3.toLowerCase()=="no"){
+}
+
+var answer3=prompt("I dont know anything about unity engine ? Y/N ")
+while (answer3.toLowerCase()!=="n" &&  answer3.toLowerCase()!=="no"){
+    alert("wrong answer")
+    answer3=prompt("I dont know anything about unity engine ? Y/N ")
+} 
+if (answer3.toLowerCase()=="n" || answer3.toLowerCase()=="no"){
     grade++;
-} if (answer4.toLowerCase()=="y" || answer4.toLowerCase()=="yes"){
+} 
+var answer4=prompt("I am a game developer ? Y/N ")
+while (answer4.toLowerCase()!=="y" && answer4.toLowerCase()!=="yes"){
+    alert("wrong answer")
+    answer4=prompt("I am a game developer ? Y/N ")
+}
+if (answer4.toLowerCase()=="y" || answer4.toLowerCase()=="yes"){
     grade++;
-}if (answer5.toLowerCase()=="n" || answer5.toLowerCase()=="no"){
+}
+var answer5=prompt("its not ayoub website ? Y/N ")
+while (answer5.toLowerCase()!=="n" && answer5.toLowerCase()!=="no"){
+    alert("wrong answer")
+    answer5=prompt("its not ayoub website ? Y/N ")
+}
+
+ if (answer5.toLowerCase()=="n" || answer5.toLowerCase()=="no"){
     grade++;
 }
 //console.log(grade);
