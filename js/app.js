ee87955115b80348b48efe6f9bf9c83cd0a491aa
lina-yousef <lina.aa96@hotmail.com>
2021-01-06 17:20:31 +0200

    Lina's update as a driver

--- js/app.js
+++ js/app.js
@@ -3,58 +3,75 @@ function doscript(){
       'use strict'
       var name = prompt("whats your name?")
       var grade=0;
-      var answer1=prompt("i am a doctor ? Y/N ")
-while(answer1.toLowerCase()!=="n" && !answer1.toLowerCase()!=="no")
-{
-    alert("wrong answer")
-    var answer1=prompt("i am a doctor ? Y/N ")
+      fun1();
+      fun2();
+      fun3(); 
+      fun4();
+      fun5();
 
-}
 
-if(answer1.toLowerCase()=="n" ||answer1.toLowerCase()=="no" ){
-
-    grade++;
-}
-var answer2=prompt("my name is ayoub ? Y/N ")
- while(answer2.toLowerCase()!=="y" && answer2.toLowerCase()!=="yes"){
-    alert("wrong answer")
-    answer2=prompt("my name is ayoub ? Y/N ")
-
-}
-if (answer2.toLowerCase()=="y"||answer2.toLowerCase()=="yes"){
-    grade++;
-}
-
-var answer3=prompt("I dont know anything about unity engine ? Y/N ")
-while (answer3.toLowerCase()!=="n" &&  answer3.toLowerCase()!=="no"){
-    alert("wrong answer")
-    answer3=prompt("I dont know anything about unity engine ? Y/N ")
-} 
-if (answer3.toLowerCase()=="n" || answer3.toLowerCase()=="no"){
-    grade++;
-} 
-      
-var answer4=prompt("I am a game developer ? Y/N ")
-while (answer4.toLowerCase()!=="y" && answer4.toLowerCase()!=="yes"){
-    alert("wrong answer")
-    answer4=prompt("I am a game developer ? Y/N ")
-}
-if (answer4.toLowerCase()=="y" || answer4.toLowerCase()=="yes"){
-    grade++;
-}
-var answer5=prompt("its not ayoub website ? Y/N ")
-while (answer5.toLowerCase()!=="n" && answer5.toLowerCase()!=="no"){
-    alert("wrong answer")
-    answer5=prompt("its not ayoub website ? Y/N ")
-}
-
- if (answer5.toLowerCase()=="n" || answer5.toLowerCase()=="no"){
-    grade++;
-}
 //console.log(grade);
 alert( name+ " grade "+grade +"/5");
 
-
 document.getElementById("name").innerHTML="Hello MR  "+name;
+
+
+function fun1() {
+    var answer1 = prompt("i am a doctor ? Y/N ");
+    while (answer1.toLowerCase() !== "n" && !answer1.toLowerCase() !== "no") {
+        alert("wrong answer");
+        var answer1 = prompt("i am a doctor ? Y/N ");
+    }
+    if (answer1.toLowerCase() == "n" || answer1.toLowerCase() == "no") {
+        grade++;
+    }
+   
+}
+
+
+function fun5() {
+    var answer5 = prompt("its not ayoub website ? Y/N ");
+    while (answer5.toLowerCase() !== "n" && answer5.toLowerCase() !== "no") {
+        alert("wrong answer");
+        answer5 = prompt("its not ayoub website ? Y/N ");
+    }
+
+    if (answer5.toLowerCase() == "n" || answer5.toLowerCase() == "no") {
+        grade++;
+    }
+}
+
+function fun4() {
+    var answer4 = prompt("I am a game developer ? Y/N ");
+    while (answer4.toLowerCase() !== "y" && answer4.toLowerCase() !== "yes") {
+        alert("wrong answer");
+        answer4 = prompt("I am a game developer ? Y/N ");
+    }
+    if (answer4.toLowerCase() == "y" || answer4.toLowerCase() == "yes") {
+        grade++;
+    }
+}
+
+function fun3() {
+    var answer3 = prompt("I dont know anything about unity engine ? Y/N ");
+    while (answer3.toLowerCase() !== "n" && answer3.toLowerCase() !== "no") {
+        alert("wrong answer");
+        answer3 = prompt("I dont know anything about unity engine ? Y/N ");
+    }
+    if (answer3.toLowerCase() == "n" || answer3.toLowerCase() == "no") {
+        grade++;
+    }
+    }
+
+function fun2() {
+    var answer2 = prompt("my name is ayoub ? Y/N ");
+    while (answer2.toLowerCase() !== "y" && answer2.toLowerCase() !== "yes") {
+        alert("wrong answer");
+        answer2 = prompt("my name is ayoub ? Y/N ");
+    }
+    if (answer2.toLowerCase() == "y" || answer2.toLowerCase() == "yes") {
+        grade++;
+    }
+}
 
 }
